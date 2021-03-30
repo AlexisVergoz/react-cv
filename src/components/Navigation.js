335fed0f69d96c2463d7a4e130d6122b5c4e7f57
Alexis Vergoz <77892858+AlexisVergoz@users.noreply.github.com>
2021-03-30 22:23:07 +0200

    Create Navigation.js

---
+++ src/components/Navigation.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+        <>
+    <div className="sidebar">
+            <div className="id">
+                <div className="idContent">
+                <img src="./media/alexisvergoz.jpg" alt="profil-pic"/>
+                <h3>Alexis Vergoz</h3>
+                
+                </div>
+            </div>
+            <div className="navigation">
+                <ul>
+                    <li>
+                    <NavLink exact to="/" activeClassName="navActive">
+                        <i className="las la-home"></i>
+                        <span>Accueil</span>
+                    </NavLink>
+                    </li>
+                    <li>
+                    <NavLink exact to="/competence" activeClassName="navActive">
+                    <i className="las la-atlas"></i>
+                        <span>Compétence</span>
+                    </NavLink>
+
+                    </li>
+                    <li>
+                    <NavLink exact to="/portfolio" activeClassName="navActive">
+                    <i class="las la-laptop"></i>
+                        <span>portfolio</span>
+                    </NavLink>
+
+                    </li>
+                    <li>
+                    <NavLink exact to="/contact" activeClassName="navActive">
+                    <i className="las la-id-card"></i>
+                        <span>Contact</span>
+                    </NavLink>
+
+                    </li>
+                </ul>
+            </div>
+        
+        <div className="socialNetwork">
+            <ul >
+                <li>
+                    <a href="https://www.linkedin.com/in/alexisvergoz/" target="_blank" rel="noopener noreferrer"><i class="lab la-linkedin"></i></a>
+                </li>
+                <li>
+                    <a href="https://github.com/AlexisVergoz" target="_blank" rel="noopener noreferrer"> <i class="lab la-github"></i></a>
+                </li>
+               
+            </ul>
+            <div className="signature">
+                <p>VergozAlexis - 2021</p>
+            </div>
+        </div>
+    </div>
+        </>
+    );
+};
+
+export default Navigation;
