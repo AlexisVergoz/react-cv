6d7ce496fd8f920b0765eae697fdae4d6d609c8f
Alexis Vergoz <77892858+AlexisVergoz@users.noreply.github.com>
2021-03-30 22:22:52 +0200

    Create Experience.js

---
+++ src/components/knowledges/Experience.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Experience = () => {
+    return (
+        <div className="experience">
+            <h3>Expérience</h3>
+            <div className="exp-1">
+                <h4>Marine Nationale</h4>
+                <h5>2013-2021</h5>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem 
+                </p>
+            </div>
+            <div className="exp-2">
+                <h4>Développeur Web Full-Stack</h4>
+                <h5>2021</h5>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+                </p>
+            </div>
+         
+        </div>
+    );
+};
+
+export default Experience;
