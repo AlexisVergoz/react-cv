9e1ae00b59cac6752cdcd05880dcf46c3ae663d6
Alexis Vergoz <77892858+AlexisVergoz@users.noreply.github.com>
2021-03-30 22:22:55 +0200

    Create Hobbies.js

---
+++ src/components/knowledges/Hobbies.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Hobbies = () => {
+    return (
+        <div className="hobbies">
+            <h3>Intérêts</h3>
+            <ul>
+                <li className="hobbies"><i class="las la-running"></i>Trail</li>
+                <li className="hobbies"><i class="las la-mountain"></i>Randonnées</li>
+                <li className="hobbies"><i class="las la-volleyball-ball"></i>volleyball-ball</li>
+                <li className="hobbies"><i class="las la-snowboarding"></i>Surf</li>
+                <li className="hobbies"><i class="las la-file-audio"></i>Musique</li>
+            </ul>
+           
+        </div>
+    );
+};
+
+export default Hobbies;
