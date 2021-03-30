658059891aabac9fce1bb430bf2220d441e3e86e
Alexis Vergoz <77892858+AlexisVergoz@users.noreply.github.com>
2021-03-30 22:22:16 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
+import home from './pages/home';
+import knowledges from './pages/knowledges';
+import portfolio from './pages/portfolio';
+import contact from './pages/contact';
+import notFound from './pages/notFound';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  return ( 
+  <>
+    <BrowserRouter >
+      <Switch>
+        <Route path="/"exact component = {home}/> 
+        <Route path="/competence"component = {knowledges}/> 
+        <Route path="/portfolio"component = {portfolio}/> 
+        <Route path="/contact"component = {contact}/> 
+        <Route component={notFound}/> 
+      </Switch> 
+    </BrowserRouter> 
+ </>
+
+
   );
-}
+};
 
 export default App;
