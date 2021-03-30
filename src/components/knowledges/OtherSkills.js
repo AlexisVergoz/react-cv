ec51da755c9057bdf41b098a749935d1af6650f7
Alexis Vergoz <77892858+AlexisVergoz@users.noreply.github.com>
2021-03-30 22:23:01 +0200

    Create OtherSkills.js

---
+++ src/components/knowledges/OtherSkills.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const OtherSkills = () => {
+    return (
+        <div className="otherSkills">
+            <h3>Autres compétences</h3>
+            <div className="list">
+                <ul>
+                    <li><i className="las la-check-square"></i>Anglais</li>
+                    <li><i className="las la-check-square"></i>Espagnol</li>
+                    <li><i className="las la-check-square"></i>GitHub</li>
+                    <li><i className="las la-check-square"></i>SEO</li>
+                </ul>
+                <ul>
+                    <li><i className="las la-check-square"></i>UI/UX Design</li>
+                    <li><i className="las la-check-square"></i>Photoshop</li>
+                    <li><i className="las la-check-square"></i>Indesign</li>
+                    <li><i className="las la-check-square"></i>illustrator</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default OtherSkills;
