9491c01deb1e74acad947413834e15e599f8fe34
Alexis Vergoz <77892858+AlexisVergoz@users.noreply.github.com>
2021-03-30 22:24:18 +0200

    Create portfolio.js

---
+++ src/pages/portfolio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const portfolio = () => {
+    return (
+        <div>
+            <Navigation/>
+            ce ci est le portfolio!!!
+        </div>
+    );
+};
+
+export default portfolio;
