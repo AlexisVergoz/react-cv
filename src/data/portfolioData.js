0885e1abd4b27d8449fc65d29d7ec4f7293070ef
Alexis Vergoz <77892858+AlexisVergoz@users.noreply.github.com>
2021-04-05 00:04:46 +0200

    Update portfolioData.js

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,56 +1,56 @@
 export const portfolioData = [
   {
     id: 1,
-    name: 'start bootstrap',
+    name: 'Start bootstrap',
     languages: ['html','css'],
     languagesIcons: ['lab la-html5','lab la-css3'],
     source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
+    info: 'Réalisation d\'une maquette avec HTML et css, exercice réalisé au début de la formation',
     picture: './media/project1..png'
   },
   {
     id: 2,
     name: 'e-commerce',
-    languages: ['javascript','react'],
-    languagesIcons: ['lab la-js-square','lab la-react'],
+    languages: ['html','css'],
+    languagesIcons: ['lab la-html5','lab la-css3'],
     source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project2.png'
+    info: 'Première evaluation, mise en oeuvre des langages HTML et CSS',
+    picture: './media/eval_1.png'
   },
   {
     id: 3,
+    name: 'RentCars',
+    languages: ['javascript','css'],
+    languagesIcons: ['lab la-js-square','lab la-css3'],
+    source: 'http://www.github.com',
+    info: 'Deuxième evaluation, mise en oeuvre de Bootstrap et javascript, site responsive',
+    picture: './media/eval_2.png'
+  },
+  {
+    id: 4,
     name: 'BeerApp',
     languages: ['javascript','angular','css'],
     languagesIcons: ['lab la-js-square','lab la-angular','lab la-css3'],
     source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project3.JPG'
-  },
-  {
-    id: 4,
-    name: 'laboriosam.fr',
-    languages: ['javascript','php','react'],
-    languagesIcons: ['lab la-js-square','lab la-php','lab la-react'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
+    info: 'Réalisation d\'une application de bière avec une API, exercice réalisé au cour de la formation',
     picture: './media/project4.JPG'
   },
   {
     id: 5,
-    name: 'repudiante.com',
-    languages: ['javascript','css'],
-    languagesIcons: ['fab fa-js', 'fab fa-css3-alt'],
+    name: 'CV Vergoz Alexis',
+    languages: ['react','javascript'],
+    languagesIcons: ['lab la-react','lab la-css3','lab la-js-square'],
     source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
+    info: 'CV réalisé entièrement en React et en autonomie',
     picture: './media/project5.JPG'
   },
   {
     id: 6,
     name: 'test.io',
-    languages: ['javascript','react', 'css'],
-    languagesIcons: ['fab fa-js','fab fa-react','fab fa-css3-alt'],
+    languages: ['javascript','php', 'css'],
+    languagesIcons: ['lab la-html5','lab la-css3','lab la-php'],
     source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
+    info: 'Projet en cour',
     picture: './media/project6.JPG'
   },
 ]
