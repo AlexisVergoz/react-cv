6e3f095bbdc85bc6b09fe60bb71a2d6ec997bf66
Alexis Vergoz <77892858+AlexisVergoz@users.noreply.github.com>
2021-04-05 00:04:37 +0200

    Update Experience.js

--- src/components/knowledges/Experience.js
+++ src/components/knowledges/Experience.js
@@ -7,13 +7,15 @@ const Experience = () => {
             <div className="exp-1">
                 <h4>Marine Nationale</h4>
                 <h5>2013-2021</h5>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem 
+                <p>Spécialiste en système informatique et télécommunication, Je m'engage en février 2013 au seins de la Marine Nationale. Après plusieurs opérations en temps que techicien du réseau informatique, je décide de mettre fin a ma carrière pour me reconvertir dans ma passion "le Web".
                 </p>
             </div>
             <div className="exp-2">
                 <h4>Développeur Web Full-Stack</h4>
                 <h5>2021</h5>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+                <p>
+                Titre RNCP Développeur Web et Web Mobile (niveau 5 équivalent BAC +2) Deux certificats CNCP (Commission Nationale de la Certification Professionnelle) :
+                Techniques de Développement web et Techniques d’Intégration Web.
                 </p>
             </div>
          
